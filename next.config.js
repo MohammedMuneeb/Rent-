30c986d083e9b40771b07ca7c35cf98d6c6cdffe
MohammedMuneeb <MohammedMuneeb@users.noreply.github.com>
2024-07-30 10:56:50 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,331 @@
+/** @type {import('next').NextConfig} */
+
+if (!process.env.APP_LOGO) {
+  console.error('APP_LOGO env variable not set :(');
+}
+if (!process.env.THEME) {
+  console.error('THEME env variable not set :(');
+}
+if (!process.env.BASE_URL) {
+  console.error('BASE_URL env variable not set :(');
+}
+if (!process.env.IMAGE_DOMAIN) {
+  console.error('IMAGE_DOMAIN env variable not set :(');
+}
+if (!process.env.RAZORPAY_KEY) {
+  console.error('RAZORPAY_KEY env variable not set :(');
+}
+
+if (!process.env.FIREBASE_API_KEY) {
+  console.error('FIREBASE_API_KEY env variable not set :(');
+}
+if (!process.env.FIREBASE_AUTH_DOMAIN) {
+  console.error('FIREBASE_AUTH_DOMAIN env variable not set :(');
+}
+if (!process.env.FIREBASE_PROJECT_ID) {
+  console.error('FIREBASE_PROJECT_ID env variable not set :(');
+}
+if (!process.env.FIREBASE_STORAGE_BUCKET) {
+  console.error('FIREBASE_STORAGE_BUCKET env variable not set :(');
+}
+if (!process.env.FIREBASE_MESSAGING_SENDER_ID) {
+  console.error('FIREBASE_MESSAGING_SENDER_ID env variable not set :(');
+}
+if (!process.env.FIREBASE_API_ID) {
+  console.error('FIREBASE_API_ID env variable not set :(');
+}
+if (!process.env.FIREBASE_MEASUREMENT_ID) {
+  console.error('FIREBASE_MEASUREMENT_ID env variable not set :(');
+}
+if (!process.env.API_BASE_URL) {
+  console.error('API_BASE_URL env variable not set :(');
+}
+
+if (!process.env.FUZZY_MIN_MATCH_RATE) {
+  console.error('FUZZY_MIN_MATCH_RATE env variable not set :(');
+}
+if (!process.env.SUPPORT_NUMBER) {
+  console.error('SUPPORT_NUMBER env variable not set :(');
+}
+if (!process.env.SHOW_RENT_WITH_TAX) {
+  console.error('SHOW_RENT_WITH_TAX env variable not set :(');
+}
+if (!process.env.CONTRACT_EXPIRY_THRESHOLD) {
+  console.error('CONTRACT_EXPIRY_THRESHOLD env variable not set :(');
+}
+if (!process.env.ORDER_QUANTITY_LIMIT) {
+  console.error('ORDER_QUANTITY_LIMIT env variable not set :(');
+}
+
+if (!process.env.PRISMIC_TERMS_OF_USE) {
+  console.error('PRISMIC_TERMS_OF_USE env variable not set :(');
+}
+if (!process.env.PRISMIC_PRIVACY_POLICY) {
+  console.error('PRISMIC_PRIVACY_POLICY env variable not set :(');
+}
+
+if (!process.env.PRISMIC_CONTACT_CUSTOM_TYPE) {
+  console.error('PRISMIC_CONTACT_CUSTOM_TYPE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_ABOUT_RENTITY) {
+  console.error('PRISMIC_ABOUT_RENTITY env variable not set :(');
+}
+
+if (!process.env.PRISMIC_CUSTOMER_REVIEWS) {
+  console.error('PRISMIC_CUSTOMER_REVIEWS env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HERO_BANNER) {
+  console.error('PRISMIC_HERO_BANNER env variable not set :(');
+}
+
+if (!process.env.PRISMIC_WHY_RENTITY_PAGE) {
+  console.error('PRISMIC_WHY_RENTITY_PAGE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_WHY_RENTITY_CUSTOM_TYPE) {
+  console.error('PRISMIC_WHY_RENTITY_CUSTOM_TYPE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOW_IT_WORKS_CUSTOM_TYPE) {
+  console.error('PRISMIC_HOW_IT_WORKS_CUSTOM_TYPE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_FEATURED_COLLECTION) {
+  console.error('PRISMIC_FEATURED_COLLECTION env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HERO_CAROUSEL_STATIC_BANNER) {
+  console.error('PRISMIC_HERO_CAROUSEL_STATIC_BANNER env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOMEPAGE_CAROUSEL_TYPE) {
+  console.error('PRISMIC_HOMEPAGE_CAROUSEL_TYPE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOMEPAGE_BROWSE_BY_BRANDS) {
+  console.error('PRISMIC_HOMEPAGE_BROWSE_BY_BRANDS env variable not set :(');
+}
+
+if (!process.env.PRISMIC_FOOTER) {
+  console.error('PRISMIC_FOOTER env variable not set :(');
+}
+
+if (!process.env.CLARITY_ANALYTICS_ID) {
+  console.error('CLARITY_ANALYTICS_ID env variable not set :(');
+}
+
+if (!process.env.WEBAPP_URL) {
+  console.error('WEBAPP_URL env variable not set :(');
+}
+if (!process.env.IS_GROUP_SEARCH) {
+  console.error('IS_GROUP_SEARCH env variable not set :(');
+}
+
+if (!process.env.PRISMIC_CUSTOMER_REVIEWS_UID) {
+  console.error('PRISMIC_CUSTOMER_REVIEWS_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOMEPAGE_HERO_BANNER_UID) {
+  console.error('PRISMIC_HOMEPAGE_HERO_BANNER_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOMEPAGE_STATIC_BANNER_UID) {
+  console.error('PRISMIC_HOMEPAGE_STATIC_BANNER_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOMEPAGE_CAROUSEL_TYPE_SECTION_UID) {
+  console.error('PRISMIC_HOMEPAGE_CAROUSEL_TYPE_SECTION_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_CONTACT_UID) {
+  console.error('PRISMIC_CONTACT_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_FOOTER_UID) {
+  console.error('PRISMIC_FOOTER_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_FEATURED_BRANDS_UID) {
+  console.error('PRISMIC_FEATURED_BRANDS_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOW_IT_WORKS_UID) {
+  console.error('PRISMIC_HOW_IT_WORKS_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_RENTITY_FEATURES_UID) {
+  console.error('PRISMIC_RENTITY_FEATURES_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_API_END_POINT) {
+  console.error('PRISMIC_API_END_POINT env variable not set :(');
+}
+
+if (!process.env.PRISMIC_CONTACT_US_PAGE) {
+  console.error('PRISMIC_CONTACT_US_PAGE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_CONTACT_US_DETAILS_UID) {
+  console.error('PRISMIC_CONTACT_US_DETAILS_UID env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOMEPAGE_COLLECTION_SECTION_TYPE) {
+  console.error('PRISMIC_HOMEPAGE_COLLECTION_SECTION_TYPE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HOMEPAGE_COLLECTION_SECTION_UID) {
+  console.error('PRISMIC_HOMEPAGE_COLLECTION_SECTION_UID env variable not set :(');
+}
+
+if (!process.env.SERVER_SIDE_CACHE_TIMER) {
+  console.error('SERVER_SIDE_CACHE_TIMER env variable not set :(');
+}
+
+if (!process.env.PRISMIC_COLLECTION_CAROUSEL_SLICE_TYPE) {
+  console.error('PRISMIC_COLLECTION_CAROUSEL_SLICE_TYPE env variable not set :(');
+}
+
+if (!process.env.PRICE_STRIKETHROUGH_PERCENTAGE) {
+  console.error('PRICE_STRIKETHROUGH_PERCENTAGE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_IMG_HOSTNAME) {
+  console.error('PRISMIC_IMG_HOSTNAME env variable not set :(');
+}
+
+if (!process.env.PRISMIC_IMG_PATHNAME) {
+  console.error('PRISMIC_IMG_PATHNAME env variable not set :(');
+}
+
+if (!process.env.RENTITY_IMG_HOSTNAME) {
+  console.error('RENTITY_IMG_HOSTNAME env variable not set :(');
+}
+
+if (!process.env.RENTITY_IMG_PATHNAME) {
+  console.error('RENTITY_IMG_PATHNAME env variable not set :(');
+}
+
+if (!process.env.PRISMIC_BLOG_TYPE) {
+  console.error('PRISMIC_BLOG_TYPE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HEADER_LINKS_TYPE) {
+  console.error('PRISMIC_HEADER_LINKS_TYPE env variable not set :(');
+}
+
+if (!process.env.PRISMIC_HEADER_LINKS_UID) {
+  console.error('PRISMIC_HEADER_LINKS_UID env variable not set :(');
+}
+
+module.exports = {
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+
+  env: {
+    logo: process.env.APP_LOGO,
+    // logo: '/images/rentity-vendor-central.png',
+
+    theme: process.env.THEME,
+    // theme: 'rentity',
+
+    apiBaseURL: process.env.BASE_URL,
+
+    restApiBaseURL: process.env.API_BASE_URL,
+
+    razorpayKey: process.env.RAZORPAY_KEY,
+
+    contractExpiryThreshold: process.env.CONTRACT_EXPIRY_THRESHOLD,
+
+    orderQuantityLimit: process.env.ORDER_QUANTITY_LIMIT,
+
+
+    firebaseConfig: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_API_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
+
+    googleAnalyticsMeasurementId: process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID,
+    googleAnalyticsStreamId: process.env.GOOGLE_ANALYTICS_STREAM_ID,
+    fuzzyMinMatchRate: process.env.FUZZY_MIN_MATCH_RATE,
+    supportNumber: process.env.SUPPORT_NUMBER,
+
+    prismicTermsOfUse: process.env.PRISMIC_TERMS_OF_USE,
+    prismicPrivacyPolicy: process.env.PRISMIC_PRIVACY_POLICY,
+
+    prismicContactCustomType: process.env.PRISMIC_CONTACT_CUSTOM_TYPE,
+    prismicAboutRentity: process.env.PRISMIC_ABOUT_RENTITY,
+    prismicCustomerReviews: process.env.PRISMIC_CUSTOMER_REVIEWS,
+    prismicHeroBanner: process.env.PRISMIC_HERO_BANNER,
+    prismicWhyRentityPage: process.env.PRISMIC_WHY_RENTITY_PAGE,
+    prismicWhyRentityCustomType: process.env.PRISMIC_WHY_RENTITY_CUSTOM_TYPE,
+    prismicHowItWorksCustomType: process.env.PRISMIC_HOW_IT_WORKS_CUSTOM_TYPE,
+    prismicFeaturedCollection: process.env.PRISMIC_FEATURED_COLLECTION,
+
+    prismicHeroCarouselStaticBanner: process.env.PRISMIC_HERO_CAROUSEL_STATIC_BANNER,
+    prismicHomepageCarouselType: process.env.PRISMIC_HOMEPAGE_CAROUSEL_TYPE,
+    prismicHomepageBrowseByBrands: process.env.PRISMIC_HOMEPAGE_BROWSE_BY_BRANDS,
+    prismicFooter: process.env.PRISMIC_FOOTER,
+
+    prismicCustomerReviewsUid: process.env.PRISMIC_CUSTOMER_REVIEWS_UID,
+    prismicHomepageHeroBannerUid: process.env.PRISMIC_HOMEPAGE_HERO_BANNER_UID,
+    prismicHomepageStaticBannerUid: process.env.PRISMIC_HOMEPAGE_STATIC_BANNER_UID,
+    prismicHomepageBrandsBannerUid: process.env.PRISMIC_HOMEPAGE_BRANDS_BANNER_UID,
+    prismicHomepageCarouselTypeSectionUid: process.env.PRISMIC_HOMEPAGE_CAROUSEL_TYPE_SECTION_UID,
+    prismicContactUid: process.env.PRISMIC_CONTACT_UID,
+    prismicFooterUid: process.env.PRISMIC_FOOTER_UID,
+    prismicFeaturedBrandsUid: process.env.PRISMIC_FEATURED_BRANDS_UID,
+    prismicHowItWorksUid: process.env.PRISMIC_HOW_IT_WORKS_UID,
+    prismicRentityFeaturesUid: process.env.PRISMIC_RENTITY_FEATURES_UID,
+    prismicContactUsPage: process.env.PRISMIC_CONTACT_US_PAGE,
+    prismicContactUsDetailsUid: process.env.PRISMIC_CONTACT_US_DETAILS_UID,
+
+    prismicHomepageCollectionSectionType: process.env.PRISMIC_HOMEPAGE_COLLECTION_SECTION_TYPE,
+    prismicHomepageCollectionSectionUid: process.env.PRISMIC_HOMEPAGE_COLLECTION_SECTION_UID,
+
+    prismicCollectionCarouselSliceType: process.env.PRISMIC_COLLECTION_CAROUSEL_SLICE_TYPE,
+
+    prismicHeaderLinksType: process.env.PRISMIC_HEADER_LINKS_TYPE,
+    prismicHeaderLinksUID: process.env.PRISMIC_HEADER_LINKS_UID,
+
+    serverSideStaleTime: process.env.SERVER_SIDE_CACHE_TIMER,
+
+    prismicApiEndPoint: process.env.PRISMIC_API_END_POINT,
+
+    showRentWithTax: process.env.SHOW_RENT_WITH_TAX,
+
+    priceStrikethroughPercentage: process.env.PRICE_STRIKETHROUGH_PERCENTAGE,
+
+
+    clarityAnalyticsId: process.env.CLARITY_ANALYTICS_ID,
+    webappURL: process.env.WEBAPP_URL,
+
+    isGroupSearch: process.env.IS_GROUP_SEARCH == 'true',
+
+    prismicBlogType: process.env.PRISMIC_BLOG_TYPE,
+  },
+
+  images: {
+    domains: [process.env.IMAGE_DOMAIN, process.env.PRISMIC_IMG_HOSTNAME, process.env.RENTITY_IMG_HOSTNAME, process.env.RENTITY_IMG_PATHNAME],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.PRISMIC_IMG_HOSTNAME,
+        pathname: process.env.PRISMIC_IMG_PATHNAME,
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.RENTITY_IMG_HOSTNAME,
+        pathname: process.env.RENTITY_IMG_PATHNAME,
+        port: '',
+      },
+    ],
+  }
+}
